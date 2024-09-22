445351cc4474d219ee84dbc2d7f01467ae163179
kmnemon <kmnemon@outlook.com>
2024-09-22 21:12:37 +0800

    challenge project 4

--- SwiftUI/project4/BetterRest/ContentView.swift
+++ SwiftUI/project4/BetterRest/ContentView.swift
@@ -10,12 +10,15 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var wakeUp = defaultWakeTime
+
     @State private var sleepAmount = 8.0
     @State private var coffeeAmount = 1
 
     @State private var alertTitle = ""
     @State private var alertMessage = ""
     @State private var showingAlert = false
+    
+    @State private var sleepTimeText = ""
 
     static var defaultWakeTime: Date {
         var components = DateComponents()
@@ -27,12 +30,12 @@ struct ContentView: View {
     var body: some View {
         NavigationStack {
             Form {
-                VStack(alignment: .leading, spacing: 0) {
-                    Text("When do you want to wake up?")
-                        .font(.headline)
-
+                Section("When do you want to wake up?") {
                     DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
                         .labelsHidden()
+                }
+                .onChange(of: wakeUp) {
+                    calculateBedtime()
                 }
 
                 VStack(alignment: .leading, spacing: 0) {
@@ -41,12 +44,30 @@ struct ContentView: View {
 
                     Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
                 }
+                .onChange(of: sleepAmount) {
+                    calculateBedtime()
+                }
 
                 VStack(alignment: .leading, spacing: 0) {
                     Text("Daily coffee intake")
                         .font(.headline)
 
                     Stepper("^[\(coffeeAmount) cup](inflect: true)", value: $coffeeAmount, in: 1...20)
+                    
+                    Picker("Daily coffee intake", selection: $coffeeAmount) {
+                        ForEach(1...20, id: \.self) {
+                            Text("\($0)")
+                        }
+                    }
+                }
+                .onChange(of: coffeeAmount) {
+                    calculateBedtime()
+                }
+                
+                
+                VStack {
+                    Text("You sleep time is: \(sleepTimeText)")
+                        .font(.largeTitle)
                 }
             }
             .navigationTitle("BetterRest")
@@ -76,12 +97,14 @@ struct ContentView: View {
 
             alertTitle = "Your ideal bedtime is…"
             alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+            
+            sleepTimeText = sleepTime.formatted(date: .omitted, time: .shortened)
         } catch {
             alertTitle = "Error"
             alertMessage = "Sorry, there was a problem calculating your bedtime."
         }
 
-        showingAlert = true
+//        showingAlert = true
     }
 }
 
