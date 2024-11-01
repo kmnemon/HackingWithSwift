824233aa65e5107ea21920f41533c331b2fe4851
kmnemon <kmnemon@outlook.com>
2024-11-01 14:40:26 +0800

    challenge5(60)

--- SwiftUI/challenge5(60)/Friends/Friends/ContentView.swift
+++ SwiftUI/challenge5(60)/Friends/Friends/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var users: [User] = [User]()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(users){ user in
+                NavigationLink {
+                    DetailView(user: user)
+                } label: {
+                    VStack(alignment: .leading){
+                        Text(user.name)
+                            .font(.headline)
+                        
+                        Text("\(user.isActive)")
+                    }
+                }
+             
+            }
+            .task{
+                await loadData()
+            }
         }
-        .padding()
+    }
+    
+    func loadData() async {
+        guard self.users.isEmpty else {
+            return
+        }
+        
+        guard let url = URL(string: "https://www.hackingwithswift.com/samples/friendface.json") else {
+            print("Invalid URL")
+            return
+        }
+        
+        do {
+            let(data, _) = try await URLSession.shared.data(from: url)
+            
+            let decoder = JSONDecoder()
+            decoder.dateDecodingStrategy = .iso8601
+            
+            if let users = try? decoder.decode([User].self, from: data){
+                self.users = users
+            }
+        } catch {
+            print("Invalid data")
+        }
     }
 }
 
--- SwiftUI/challenge5(60)/Friends/Friends/User.swift
+++ SwiftUI/challenge5(60)/Friends/Friends/User.swift
@@ -7,21 +7,21 @@
 
 import Foundation
 
-struct User: Codable {
-    struct Friend: Codable {
-        var id: UUID
-        var name: String
+struct User: Codable, Identifiable {
+    struct Friend: Codable, Identifiable {
+        let id: UUID
+        let name: String
     }
     
-    var id: UUID
-    var isActive: Bool
-    var name: String
-    var age: Int
-    var company: String
-    var email: String
-    var address: String
-    var about: String
-    var registered: Date
-    var tags: [String]
-    var friends: [Friend]
+    let id: UUID
+    let isActive: Bool
+    let name: String
+    let age: Int
+    let company: String
+    let email: String
+    let address: String
+    let about: String
+    let registered: Date
+    let tags: [String]
+    let friends: [Friend]
 }
